e1c561bca1ada8cc3ee1c02063567530e021fe75
duskje <duskje@protonmail.com>
2021-08-24 19:17:56 -0300

    Updated the models

--- flashcards/models.py
+++ flashcards/models.py
@@ -1,18 +1,25 @@
 from django.db import models
+from django.conf import settings
 
 
 class Deck(models.Model):
     owner = models.ForeignKey(
-        'auth.User',
+        settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
+        related_name='decks',
     )
 
-    title = models.CharField(max_length=25)
+    title = models.CharField(max_length=200)
     last_seen_date = models.DateTimeField()
 
 
 class Flashcard(models.Model):
-    parent_deck = models.ForeignKey(Deck, on_delete=models.CASCADE)
+    parent_deck = models.ForeignKey(
+        Deck,
+        on_delete=models.CASCADE,
+        related_name='flashcards',
+    )
+
     last_seen_date = models.DateTimeField()
 
     front_text = models.CharField(max_length=200)

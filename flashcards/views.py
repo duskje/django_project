602ab41873ebdbc69c7e05d65029e33b9fd4dabf
Sam Rose <hello@samwho.dev>
2021-08-25 20:16:33 +0100

    Add a logout link to the homepage.

--- flashcards/views.py
+++ flashcards/views.py
@@ -1,7 +1,7 @@
+from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from django.http.response import HttpResponse
 
 
 @login_required(login_url="/login/")
 def home(request):
-    return HttpResponse(f"Hello, {request.user.username}")
+    return render(request, "home.html", context={"request": request})

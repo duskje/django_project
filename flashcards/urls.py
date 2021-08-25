1f83e5659393369ed40260431fce718e69fa9dbb
Sam Rose <hello@samwho.dev>
2021-08-25 20:06:59 +0100

    Get login and logout working.
    These aren't going to win any beauty contests, but hey, they work!

--- flashcards/urls.py
+++ flashcards/urls.py
@@ -1,3 +1,12 @@
 from django.urls import path
-
 from flashcards import views
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    path("", views.home),
+    path(
+        "login/",
+        auth_views.LoginView.as_view(redirect_authenticated_user=True, success_url="/"),
+    ),
+    path("logout/", auth_views.LogoutView.as_view(next_page="/")),
+]
--- flashcards/views.py
+++ flashcards/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.http.response import HttpResponse
 
-# Create your views here.
+
+@login_required(login_url="/login/")
+def home(request):
+    return HttpResponse(f"Hello, {request.user.username}")

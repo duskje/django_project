0cd3b87977ba8c1c9fd8645187f1f32ad6c951a2
duskje <duskje@protonmail.com>
2021-08-26 18:05:52 -0300

    Made the login and registration views

--- CoolProject/settings.py
+++ CoolProject/settings.py
@@ -102,6 +102,8 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# AUTH_USER_MODEL = 'flashcards.User'
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
--- flashcards/views.py
+++ flashcards/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+from django.views.generic import FormView, TemplateView
+
+from django.urls import reverse_lazy
+
+from . import forms
+
+
+class UserDecksView(LoginRequiredMixin, TemplateView):
+    template_name = 'user_decks.html'
+
+
+class RegisterView(FormView):
+    template_name = 'register.html'
+    form_class = forms.RegisterForm
+    success_url = reverse_lazy('decks')
+
+    def form_valid(self, form):
+        user = get_user_model()
+
+        user.objects.create_user(
+            username=form.cleaned_data.get('username'),
+            password=form.cleaned_data.get('password'),
+            email=form.cleaned_data.get('email'),
+        )
+
+        return super().form_valid(form)
+
+
